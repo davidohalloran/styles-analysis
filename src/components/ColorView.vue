1de285ad124e5e67891d32d21d073b25cf08fd2e
David O'Halloran <contact@davidohalloran.com>
2022-07-22 23:49:38 +0100

    chore: remove defineProps imports

--- src/components/ColorView.vue
+++ src/components/ColorView.vue
@@ -1,5 +1,4 @@
 <script setup lang="ts">
-import { defineProps } from 'vue'
 import type { ColorInfo } from '@shared'
 
 defineProps<{ color: ColorInfo }>()
--- src/components/FileLink.vue
+++ src/components/FileLink.vue
@@ -1,5 +1,5 @@
 <script setup lang="ts">
-import { defineProps, computed } from 'vue'
+import { computed } from 'vue'
 import { editor } from '~/logic/preferences'
 import { isStaticHost } from '~/env'
 
